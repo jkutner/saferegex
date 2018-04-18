bc338400469f3b936024989832792215baf238bd
Joe Kutner <jpkutner@gmail.com>
2018-04-18 08:25:34 -0500

    Added a release script [skip ci]

---
+++ release.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Releasing to Maven Central can be confusing. So this will partially automate
+# the process.
+# http://central.sonatype.org/pages/apache-maven.html
+
+# First, make sure you have your gpg key setup
+# http://central.sonatype.org/pages/working-with-pgp-signatures.html
+
+# Then make sure your ~/.m2/settings.xml has this stuff in it:
+#
+# <server>
+#   <id>ossrh</id>
+#     <username>youruser</username>
+#     <password>XXXXXXXXXX</password>
+#   </server>
+# </servers>
+#
+# <profiles>
+#   <profile>
+#     <id>ossrh</id>
+#     <activation>
+#     <activeByDefault>true</activeByDefault>
+#     </activation>
+#     <properties>
+#       <gpg.executable>gpg</gpg.executable>
+#       <gpg.passphrase>XXXXXXXXXX</gpg.passphrase>
+#     </properties>
+#   </profile>
+# </profiles>
+
+
+./mvnw release:clean release:prepare -DdryRun
+
+./mvnw release:clean release:prepare
+
+./mvnw release:perform
